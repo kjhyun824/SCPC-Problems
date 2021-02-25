e3f8c8ee0aff8a9cfddf6f8ddef6986545d11b95
Junghyun <woori4829@gmail.com>
2021-02-25 11:11:08 +0900

    Change vector to boolean array & map

--- practice/chemSentence/chemSentence.cpp
+++ practice/chemSentence/chemSentence.cpp
@@ -17,6 +17,9 @@ vector<string> elements = {
 		"Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Ac",
 		"Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"};
 
+bool elements_exists[729] = { false };
+map<string, int> string_num;
+
 std::string lower_function(std::string input) {
 	std::locale loc;
 	std::string output;
@@ -33,7 +36,7 @@ bool isChemSentence(std::string input) {
 
 	bool result = false;
 
-	if(input.size() >= 2 && std::find(elements.begin(), elements.end(), input.substr(0,2)) != elements.end()) {
+	if(input.size() >= 2 && elements_exists[string_num[input.substr(0,2)]]) {
 		result = isChemSentence(input.substr(2, input.size() - 2));
 
 		if(result) {
@@ -41,22 +44,62 @@ bool isChemSentence(std::string input) {
 		}
 	} 
 
-	if(std::find(elements.begin(), elements.end(), input.substr(0,1)) != elements.end()) {
+	if(elements_exists[string_num[input.substr(0,1)]]) {
 		result = isChemSentence(input.substr(1, input.size() - 1));
 	}
 
 	return result;
 }
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(false); 
-    cin.tie(NULL); 
-    cout.tie(NULL);
+int pow(int base, int pow) {
+	int result = 1;
 
-	std::locale loc;
-	for(int i = 0; i < elements.size(); i++) {
-		elements[i] = lower_function(elements[i]);
+	for(int i = 0; i < pow; i++) result *= base;
+
+	return result;
+}
+
+int string_to_int(string input) {
+	int output = 0;
+	for(int i = 0; i < input.length(); i++) {
+		int index = input.length() - i - 1;
+		output += (((int) input[i] - (int) 'a' + 1) * pow(26, index));
 	}
+
+	return output;
+}
+
+void init() {
+	std::locale loc;
+
+	for(int i = 0; i < 27; i++) {
+		for(int j = 0; j < 26; j++) {
+			string str = "";
+			if(i == 0) {
+				str += (char) (j + (int) 'a');
+			} else {
+				str += (char) (i + (int) 'a' - 1);
+				str += (char) (j + (int) 'a');
+			}
+
+			string_num[str] = (j + 1) + (i * pow(26, 1));
+		}
+	}
+
+	for(int i = 0; i < elements.size(); i++) {
+		string output = lower_function(elements[i]);
+		int s_to_i = string_num[output];
+
+		elements_exists[s_to_i] = true;
+	}
+}
+
+int main(int argc, char** argv) {
+  ios::sync_with_stdio(false); 
+  cin.tie(NULL); 
+  cout.tie(NULL);
+
+	init();
 
 	int T, test_case;
 	cin >> T;
